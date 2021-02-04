077bf1ec0db66b814aa6646ced4bdff35bafd6fc
Jack <jackbaude@gmail.com>
2021-02-04 11:32:42 -0600

    TODO - fix archive button style

--- src/components/Archive/Archive.tsx
+++ src/components/Archive/Archive.tsx
@@ -1,8 +1,8 @@
 //TODO add pagnation
 import React, {memo, useEffect, useMemo, useState} from "react"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faDownload, faFile, faUpload} from "@fortawesome/free-solid-svg-icons";
-import NotAuthenticated from "../ErrorPages/NotAuthenticated";
+import {Table, Th, Tr, Td, Thead, Tbody, Input, FormControl, Alert, AlertIcon} from "@chakra-ui/react"
+import { FiDownload } from "react-icons/fi"
+import {Button} from "../RichUtils";
 const Archive = () => {
     const [metadata, setMetadata] = useState([])
     //default state of the fetch getPost is loading
@@ -38,39 +38,29 @@ const Archive = () => {
     };
     const postLink = useMemo(() => Object.entries(metadata).map
         (([id, {name, size, created, link}]) => (
-                <tr>
-                    <th scope="row">{name}</th>
-                    <td>{created}</td>
-                    <td>{size} kb</td>
-                    <td><a href={"/api" + link}><FontAwesomeIcon icon={faDownload} size={"lg"}/></a></td>
-                </tr>
+                <Tr>
+                    <Td>{name}</Td>
+                    <Td>{created}</Td>
+                    <Td>{size} kb</Td>
+                    <Td><a href={"/api" + link}><FiDownload/></a></Td>
+                </Tr>
             )
         ),
         [metadata]);
-    const formButtons = () => {
+    const FormButton = () => {
         if (checkedAuth && !authed) {
-            return
+            return <div/>
         }
         const file_ext = (file.split('.').pop())
         if (file === "") {
-            return <label htmlFor="getFile" className={"btn btn-lg file-input-button"}><FontAwesomeIcon
-                icon={faFile}/> Select File To Upload</label>
+            return <label htmlFor="getFile" className={"btn btn-lg file-input-button"}> Select File To Upload</label>
         } else if (file_ext !== "litematic" && file_ext !== "schematic" && file_ext !== "nbt") {
-            return (<div>
-                <div className="alert alert-danger alert-dismissible fade show" role="alert">
-                    Woah There! We only accept <strong>.litematic .schematic </strong>and <strong>.nbt </strong>files
-                    <button type="button" className="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <label htmlFor="getFile" className={"btn btn-lg file-input-button"}><FontAwesomeIcon
-                    icon={faFile}/> Select File To Upload</label>
-            </div>)
+            return (
+                <div/>
+            )
         } else {
-            return <button className={"btn btn-lg file-input-button"} type={"submit"} value={"submit"}><FontAwesomeIcon
-                icon={faUpload}/> {file}</button>
+            return <Button className={"btn btn-lg file-input-button"} type={"submit"} value={"submit"}> {file}</Button>
         }
-
     }
     // if the post is still loading just render a loading bar
     if (fetchState === "loading") {
@@ -85,33 +75,34 @@ const Archive = () => {
     //if we caught a error send a failed message
     else if (fetchState === "failed") {
         return (
-            <div className="alert alert-danger" role="alert">Sorry Looks like something is going wrong. Is the API down?
-                Check with Jakku on the Discord.</div>
+            <Alert status="error">
+                <AlertIcon />
+                Looks like we can not reach the API.
+            </Alert>
+
         )
     }
     else return (
         <div>
-            <form action={"/api/__archive-upload__"} encType={"multipart/form-data"} method={"POST"}>
-                <input id={"getFile"} type={"file"} name={"file"} className={"file-input"} onChange={(event) => {
+            <FormControl action={"/api/__archive-upload__"} encType={"multipart/form-data"} method={"POST"}>
+                <Input id={"getFile"} type={"file"} name={"file"} onChange={(event) => {
                     setFile(event.target.files[0].name)
                 }}/>
-                {formButtons()}
-
-
-            </form>
-            <table className="table table-sm table-borderless">
-                <thead>
-                <tr>
-                    <th scope="col">Name</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Size</th>
-                    <th scope="col">Download</th>
-                </tr>
-                </thead>
-                <tbody>
+                <FormButton/>
+            </FormControl>
+            <Table variant={"striped"}>
+                <Thead>
+                <Tr>
+                    <Th>Name</Th>
+                    <Th>Last</Th>
+                    <Th>Size</Th>
+                    <Th>Download</Th>
+                </Tr>
+                </Thead>
+                <Tbody>
                 {postLink}
-                </tbody>
-            </table>
+                </Tbody>
+            </Table>
         </div>
     );
 }
